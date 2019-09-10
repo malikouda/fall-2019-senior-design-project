bda5be0613f3e536e9396f8c05c401adcaf32164
Malik <malikouda@gmail.com>
2019-09-10 01:08:05 -0500

    Basic character movement implemented

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+    public float moveSpeed = 6f;
+    public float rotateSpeed = 10f;
+
+    Rigidbody rb;
+    Vector3 moveDirection;
+    float inputAmount;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        moveDirection = Vector3.zero;
+
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        Vector3 combinedInput = new Vector3(horizontal, 0, vertical);
+
+        moveDirection = new Vector3(combinedInput.normalized.x, 0, combinedInput.normalized.z);
+
+        float inputMagnitude = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
+        inputAmount = Mathf.Clamp01(inputMagnitude);
+
+        Quaternion rot = Quaternion.LookRotation(moveDirection);
+        Quaternion targetRotation = Quaternion.Slerp(transform.rotation, rot, Time.fixedDeltaTime * inputAmount * rotateSpeed);
+        transform.rotation = targetRotation;
+    }
+
+    private void FixedUpdate() {
+        rb.velocity = (moveDirection * moveSpeed * inputAmount);
+    }
+}
