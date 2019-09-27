53ad5d897f9d6093a71df8aef17281824ffe9aa2
Malik <malikouda@gmail.com>
2019-09-27 17:10:34 -0500

    Jim is less than human
    Signed-off-by: Malik <malikouda@gmail.com>

---
+++ Assets/Scripts/guardMovement.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class guardMovement : MonoBehaviour {
+
+
+    public float threshold;
+
+    private List<GameObject> waypoints;
+    private List<Vector3> patrol;
+    private int index;
+    private NavMeshAgent mAgent;
+    private int direction = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mAgent = gameObject.GetComponent<NavMeshAgent>();
+        patrol = new List<Vector3>();
+        waypoints = new List<GameObject>();
+        foreach (GameObject waypoint in GameObject.FindGameObjectsWithTag("Waypoint")) 
+        {
+            waypoints.Add(waypoint);
+        }
+        GameObject current = gameObject;
+        int total = waypoints.Count;
+        while (patrol.Count < total) {
+            GameObject next = findClosestPoint(current);
+            patrol.Add(next.transform.position);
+            waypoints.Remove(current);
+            current = next;
+        }
+
+        Vector3 lastpoint = transform.position;
+        foreach (Vector3 point in patrol) 
+        {
+            Debug.DrawLine(lastpoint, point,Color.red,100);
+            lastpoint = point;
+        }
+    }
+
+    GameObject findClosestPoint(GameObject current) 
+    {
+        float minDist = Mathf.Infinity;
+        GameObject point = null;
+        foreach (GameObject waypoint in waypoints) 
+        {
+            if (waypoint != current) 
+            {
+                float dist = Vector3.Distance(current.transform.position, waypoint.transform.position);
+                if (dist < minDist) {
+                    minDist = dist;
+                    point = waypoint;
+                }
+            }
+
+        }
+
+        return point;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, mAgent.destination) < threshold) 
+        {
+            index += 1 * direction;
+            if (index >= patrol.Count) 
+            {
+                index = patrol.Count - 1;
+                direction = -1;
+            }
+
+            if (index < 0) 
+            {
+                index = 0;
+                direction = 1;
+            }
+
+            mAgent.destination = patrol[index];
+        }
+    }
+}
---
+++ Assets/Scripts/patrolCreation.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class patrolCreation : MonoBehaviour
+{
+
+    public int resolution;
+
+    public float XCoord;
+    public float ZCoord;
+
+    public static List<Vector3> points;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        if (points == null) 
+        {
+            points = new List<Vector3>();
+            for (float x = -XCoord; x <= XCoord; x += ((XCoord) / resolution)) {
+                for (float z = -ZCoord; z <= ZCoord; z += ((ZCoord) / resolution)) {
+                    RaycastHit hit;
+                    if (Physics.Raycast(new Vector3(x, 10, z), Vector3.down, out hit)) {
+                        if (hit.collider.gameObject.tag == "Ground") {
+                            points.Add(hit.point);
+                        }
+                    }
+                }
+
+            }
+
+            List<Vector3> maxPoints = new List<Vector3>();
+            for (int i = 0; i < points.Count; i++) {
+                int index = 0;
+                float maxDistance = 0;
+                for (int x = 0; x < points.Count; x++) 
+                {
+
+                    if (!Physics.Linecast(points[i], points[x])) 
+                    {
+                        float dist = Vector3.Distance(points[i], points[x]);
+                        if (dist > maxDistance) 
+                        {
+                            maxDistance = dist;
+                        }else 
+                        {
+                            points.RemoveAt(index);
+                        }
+                    }
+                    maxPoints.Add(new Vector3(points[i].x + points[index].x / 2, points[i].y + points[index].y / 2, points[i].z + points[index].z / 2));
+                    points.RemoveAt(i);
+                }
+                
+            }
+
+            foreach (Vector3 point in maxPoints) 
+            {
+                Debug.DrawRay(point, Vector3.up,Color.blue,10);
+            }
+        }
+       
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireCube(Vector3.zero, new Vector3(XCoord, 10, ZCoord));
+    }
+
+}
