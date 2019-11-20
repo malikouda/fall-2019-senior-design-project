134fbc3f1b1ff5acf2520536c9fd5f6b4408cdac
Torry Mathis <to.mathis21@gmail.com>
2019-11-19 18:37:43 -0600

    Added loading screen

--- Assets/Scripts/testing.cs
+++ Assets/Scripts/testing.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem.UI;
 
 public class testing : MonoBehaviour
 {
     public GameObject screen;
     public Animator anim;
+    public string trigger;
     // Start is called before the first frame update
     void Start()
     {
+        
         screen.SetActive(true);
-        anim.SetTrigger("Win");
+        anim.SetTrigger(trigger);
     }
 }
